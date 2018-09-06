f712b713e674831099311532e0e7e9c4d6040019
Vishal Rakholiya <vrakholiya007@gmail.com>
2018-09-06 20:24:12 +0530

    changes

--- src/services/login.ts
+++ src/services/login.ts
@@ -13,8 +13,7 @@ export class LoginProvider {
   }
 
   ValidateUser(id, pass) {
-    return new Promise((resolve,reject)=>{
-      console.log(`${this.url}/Home/login?UserName=${id}&Password=${pass}`)
+    return new Promise((resolve,reject)=>{      
       this.http.get(`${this.url}/Home/login?UserName=${id}&Password=${pass}`).subscribe(res => {
         resolve(res);
       });
@@ -22,9 +21,20 @@ export class LoginProvider {
   }
   GetAllMessages(userid){
     return new Promise((resolve,reject)=>{
+      console.log(`${this.url}/Home/GetMessage?UserId=${userid}`);
       this.http.get(`${this.url}/Home/GetMessage?UserId=${userid}`).subscribe(res => {
         resolve(res);
       });
     })
+  }
+  MsgClickCount(msgId){
+    return new Promise((resolve,reject)=>{     
+     
+       
+      this.http.get(`${this.url}/Home/SetMessageCount?idMessageSerial=${msgId}`).subscribe(res => {
+        console.log('count',res);
+        resolve(res);
+      });
+    });
   }
 }
